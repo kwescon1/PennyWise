f6cfa81b7fc91d9f80029b071505cba15ce6263b
Odame <kwesi.danquah@daaseng.com>
2024-09-02 13:34:59 +0100

    feat: Add localization language lines for common application messages

---
+++ pennywise_backend/lang/en/app.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in application for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'registration_successful' => 'User registered successfully',
+    'operation_successful' => 'Operation successful',
+    'resource_created' => 'Resource created',
+    'resource_not_found' => 'Resource not found',
+
+];
