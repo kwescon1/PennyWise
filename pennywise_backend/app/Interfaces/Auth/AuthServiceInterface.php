56eaab145aa37c728c1bd8469c0f2f5ce40cb069
Odame <kwesi.danquah@daaseng.com>
2024-09-05 17:31:17 +0100

    refactor: Update sendOtp method to allow dynamic OTP type
    - Modified sendOtp to accept an optional $type parameter for flexibility in OTP types (defaulting to verification code).
    - Allows reuse of the method for different OTP purposes (e.g., password reset, verification).

--- pennywise_backend/app/Interfaces/Auth/AuthServiceInterface.php
+++ pennywise_backend/app/Interfaces/Auth/AuthServiceInterface.php
@@ -3,12 +3,13 @@
 namespace App\Interfaces\Auth;
 
 use App\Models\User;
+use App\Enums\Auth\OtpType;
 
 interface AuthServiceInterface
 {
 
     public function register(array $data, string $code): array;
     public function login(User $user): array;
-    public function sendOtp(User $user, string $code): void;
+    public function sendOtp(User $user, string $code, $type = OtpType::VERIFICATION_CODE): void;
     public function verifyOtp(User $user, int $otp): User;
 }
