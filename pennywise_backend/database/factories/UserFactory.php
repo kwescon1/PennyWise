c10a95adb9d70b5320af7bb35136c9af6367d9d7
Odame <kwesi.danquah@daaseng.com>
2024-09-11 11:02:36 +0100

    Fix: Update UserFactory to generate valid UUID and realistic usernames

--- pennywise_backend/database/factories/UserFactory.php
+++ pennywise_backend/database/factories/UserFactory.php
@@ -14,7 +14,7 @@ class UserFactory extends Factory
     /**
      * The current password being used by the factory.
      */
-    protected static ?string $password;
+    protected static ?string $password = null;
 
     /**
      * Define the model's default state.
@@ -24,10 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
+            'username' => fake()->userName(),
+            'uuid' => fake()->uuid(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('password'), // Use a static hashed password
             'remember_token' => Str::random(10),
         ];
     }
@@ -37,7 +40,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
