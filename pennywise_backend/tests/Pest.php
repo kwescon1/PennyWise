31df47292219a173fe79e37eb3a2a504a08e2def
Odame <kwesi.danquah@daaseng.com>
2024-09-12 01:48:44 +0100

    chore: update pest.php configuration for improved test setup

--- pennywise_backend/tests/Pest.php
+++ pennywise_backend/tests/Pest.php
@@ -1,32 +1,47 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 /*
 |--------------------------------------------------------------------------
 | Test Case
 |--------------------------------------------------------------------------
 |
-| The closure you provide to your test functions is always bound to a specific PHPUnit test
-| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
-| need to change it using the "pest()" function to bind a different classes or traits.
+| You may extend TestCase to apply specific setup for all tests in specific
+| directories or globally. Pest uses `beforeEach` to make configurations available.
 |
 */
 
 // Apply TestCase to both Feature and Unit tests
-pest()->extend(Tests\TestCase::class)->in('Feature', 'Unit');
+pest()->extend(TestCase::class)->in('Feature', 'Unit');
 
 // Apply RefreshDatabase only to Feature tests (or other specific test directories that need database)
 uses(RefreshDatabase::class)->in('Feature');
 
 /*
 |--------------------------------------------------------------------------
+| Scoped Setup for Feature Tests
+|--------------------------------------------------------------------------
+|
+| To make the route names available only for Feature tests, we use the `beforeEach`
+| hook inside the `uses()` function to limit it to the `Feature` test directory.
+|
+*/
+
+uses()->beforeEach(function () {
+    // Make route names available globally for feature tests only
+    $this->routeNames = config('routes');
+})->in('Feature');
+
+/*
+|--------------------------------------------------------------------------
 | Expectations
 |--------------------------------------------------------------------------
 |
-| When you're writing tests, you often need to check that values meet certain conditions. The
-| "expect()" function gives you access to a set of "expectations" methods that you can use
-| to assert different things. Of course, you may extend the Expectation API at any time.
+| When you're writing tests, you often need to check that values meet certain conditions.
+| The "expect()" function gives you access to a set of "expectations" methods that you
+| can use to assert different things.
 |
 */
 
@@ -39,9 +54,8 @@ expect()->extend('toBeOne', function () {
 | Functions
 |--------------------------------------------------------------------------
 |
-| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
-| project that you don't want to repeat in every file. Here you can also expose helpers as
-| global functions to help you to reduce the number of lines of code in your test files.
+| Here you can expose helpers as global functions to help you reduce the number
+| of lines of code in your test files.
 |
 */
 
