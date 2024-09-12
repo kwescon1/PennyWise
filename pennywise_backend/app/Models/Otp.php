a44025dde6b1d3ada24b2906c0a7de9bc1851303
Odame <kwesi.danquah@daaseng.com>
2024-09-13 00:08:12 +0100

    Add relevant scopes in Otp model

--- pennywise_backend/app/Models/Otp.php
+++ pennywise_backend/app/Models/Otp.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Enums\Auth\OtpType;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,4 +20,39 @@ class Otp extends Model
         "type" => OtpType::class,
         "expires_at" => "datetime",
     ];
+
+    /**
+     * Scope to get active OTPs of a certain type.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $type
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeActiveOtps(Builder $query, OtpType $type): Builder
+    {
+        return $query->whereIsActive(self::STATUS_ACTIVE)->whereType($type);
+    }
+
+    /**
+     * Scope a query to only include records created within a time frame.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  int  $minutes  The number of minutes within which records should have been created.
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeRecent(Builder $query, $minutes): Builder
+    {
+        return $query->where('created_at', '>=', $minutes);
+    }
+
+    /**
+     * Scope to get OTPs that have not expired.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeNotExpired(Builder $query): Builder
+    {
+        return $query->where('expires_at', '>', now());
+    }
 }
