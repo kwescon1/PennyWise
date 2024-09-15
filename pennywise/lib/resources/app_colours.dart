d4f6e364950773d2995004d0a2344a61d3260f29
Odame <kwesi.danquah@daaseng.com>
2024-09-15 23:33:24 +0100

    Added color definitions for app theme

---
+++ pennywise/lib/resources/app_colours.dart
@@ -0,0 +1,8 @@
+// This class defines the main color scheme for the application.
+
+import 'dart:ui';
+
+class AppColours{
+  static Color primaryColour = const Color(0xFF7F3DFF); // Primary color used across the app
+  static Color primaryColourLight = const Color(0xFFEEE5FF); // Lighter shade of the primary color
+  static Color bgColor = const Color(0xFFFFFFFF); // Background color for the app
