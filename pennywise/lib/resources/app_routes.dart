b20dff29e901ca60246c235384e1265477af46b3
Odame <kwesi.danquah@daaseng.com>
2024-09-15 23:30:42 +0100

    Added application routes for splash and walkthrough screens.

---
+++ pennywise/lib/resources/app_routes.dart
@@ -0,0 +1,12 @@
+/// This class defines the application's routes for navigation.
+///
+/// Each route is represented as a static string, which can be used
+/// throughout the Flutter app to navigate between different screens.
+///
+/// Example usage:
+/// Navigator.pushNamed(context, AppRoutes.splash);
+///
+class AppRoutes {
+  static String splash = '/';
+  static String walkthrough = '/walkthrough';
+}
