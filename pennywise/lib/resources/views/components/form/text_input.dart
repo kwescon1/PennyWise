ae9f613567628c889c2d05ae192fb89befc14a8b
Odame <kwesi.danquah@daaseng.com>
2024-09-23 10:36:41 +0100

    Update text input component functionalities

--- pennywise/lib/resources/views/components/form/text_input.dart
+++ pennywise/lib/resources/views/components/form/text_input.dart
@@ -8,15 +8,26 @@ class TextInputComponent extends StatefulWidget {
   final bool isEnabled;
   final bool isRequired;
   final String label;
+  final String? error;
   final bool isPassword;
   final TextEditingController textEditingController;
-  final ValueChanged<String> ?onFieldSubmitted;
-  final TextInputAction ?textInputAction;
+  final ValueChanged<String>? onFieldSubmitted;
+  final TextInputAction? textInputAction;
   final FocusNode? focusNode;
   final TextInputType? textInputType;
 
   const TextInputComponent(
-      {super.key, required this.label, this.isPassword = false, required this.textEditingController, this.onFieldSubmitted, this.textInputAction, this.focusNode, this.textInputType, this.isRequired = false, this.isEnabled = true});
+      {super.key,
+      required this.label,
+      this.isPassword = false,
+      required this.textEditingController,
+      this.onFieldSubmitted,
+      this.textInputAction,
+      this.focusNode,
+      this.textInputType,
+      this.isRequired = false,
+      this.isEnabled = true,
+      this.error});
 
   @override
   State<TextInputComponent> createState() => _TextInputComponentState();
@@ -30,18 +41,25 @@ class _TextInputComponentState extends State<TextInputComponent> {
     return TextFormField(
       enabled: widget.isEnabled,
       autovalidateMode: AutovalidateMode.onUserInteraction,
-      validator: (value){
-        if(!widget.isRequired) return null;
-        if(value == null || value.isEmpty){
-          return '${widget.label} ${AppStrings.fieldIsRequired}';}
+      validator: (value) {
+        if (!widget.isRequired) return null;
+        if (value == null || value.isEmpty) {
+          return '${widget.label} ${AppStrings.fieldIsRequired}';
+        }
+
+        if (!widget.isPassword && value.trim().isEmpty) {
+          return AppStrings.inputIsRequired.replaceAll(":input", widget.label);
+        }
         return null;
       },
+      controller: widget.textEditingController,
       keyboardType: widget.textInputType,
       focusNode: widget.focusNode,
       onFieldSubmitted: widget.onFieldSubmitted,
       textInputAction: widget.textInputAction ?? TextInputAction.next,
       obscureText: (widget.isPassword && showPassword),
       decoration: InputDecoration(
+        errorText: widget.error,
         labelText: widget.label,
         labelStyle: TextStyle(color: AppColours.light20),
         border: OutlineInputBorder(
@@ -54,21 +72,21 @@ class _TextInputComponentState extends State<TextInputComponent> {
             borderRadius: BorderRadius.circular(16),
             borderSide: BorderSide(color: AppColours.primaryColour)),
         suffixIcon: widget.isPassword
-            ? InkWell(
-          onTap: togglePassword,
-          child: Icon(
-            showPassword
-                ? Icons.visibility_off_outlined
-                : Icons.visibility_outlined,
-            color: AppColours.light20,
-          ),
-        )
+            ? IconButton(
+                onPressed: togglePassword,
+                icon: Icon(
+                  showPassword
+                      ? Icons.visibility_off_outlined
+                      : Icons.visibility_outlined,
+                  color: AppColours.light20,
+                ),
+              )
             : AppSpacing.empty(),
       ),
     );
   }
 
   void togglePassword() => setState(() {
-    showPassword = !showPassword;
-  });
+        showPassword = !showPassword;
+      });
 }
